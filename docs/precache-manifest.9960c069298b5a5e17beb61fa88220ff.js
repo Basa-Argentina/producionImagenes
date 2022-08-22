9255482a9ca8835cb2811db89f6d70462be54d56
Basa-Argentina <basaargentina2013@gmail.com>
2022-08-22 10:03:40 -0300

    deploy
    deploy

---
+++ docs/precache-manifest.9960c069298b5a5e17beb61fa88220ff.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a44b65c602fa2d6fb65fbe1ee92aed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c0c02be696ef3b1b0ef",
+    "url": "./static/css/3.5a943c09.chunk.css"
+  },
+  {
+    "revision": "536ebf21454eec70e477",
+    "url": "./static/css/main.41b6c2c3.chunk.css"
+  },
+  {
+    "revision": "ccdd0bd6741e09541d8d",
+    "url": "./static/js/0.0aa6272e.chunk.js"
+  },
+  {
+    "revision": "eddb53691628a667e6498c6230cd8119",
+    "url": "./static/js/0.0aa6272e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2905c35cd0c8b4c8f09f",
+    "url": "./static/js/10.a5f41f13.chunk.js"
+  },
+  {
+    "revision": "0c0c02be696ef3b1b0ef",
+    "url": "./static/js/3.bab25043.chunk.js"
+  },
+  {
+    "revision": "62abe90acaafd7c9f0a476b1ffe43eec",
+    "url": "./static/js/3.bab25043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ceb98b38783e59415b1",
+    "url": "./static/js/4.a7e227b4.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/4.a7e227b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121b7adf11cfef6dd117",
+    "url": "./static/js/5.eeb78c6f.chunk.js"
+  },
+  {
+    "revision": "536950dbfc8788735deaf56f21469aea",
+    "url": "./static/js/5.eeb78c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10e56f41c62d7c6a107",
+    "url": "./static/js/6.7668d156.chunk.js"
+  },
+  {
+    "revision": "908eadc992b74ba3b382",
+    "url": "./static/js/7.ce8b789e.chunk.js"
+  },
+  {
+    "revision": "0853000161898f0855cd262923b84127",
+    "url": "./static/js/7.ce8b789e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2728ebf4f405dd2937b",
+    "url": "./static/js/8.7ed1493b.chunk.js"
+  },
+  {
+    "revision": "b8d38f354bd60e5be553",
+    "url": "./static/js/9.2e709d19.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "./static/js/9.2e709d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536ebf21454eec70e477",
+    "url": "./static/js/main.114e5a31.chunk.js"
+  },
+  {
+    "revision": "bfcc85da76a39dac97e4",
+    "url": "./static/js/runtime-main.64024bd0.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "./static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "ac393e29de3d69121e774fefe224fb83",
+    "url": "./static/media/logoBasaNuevo.ac393e29.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "./static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "./static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "./static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "./static/media/primeicons.e01fd413.eot"
+  }
+]);
---
+++ docs/static/js/6.7668d156.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbasa-digital-app"]=this["webpackJsonpbasa-digital-app"]||[]).push([[6],{498:function(a,i){},526:function(a,i){},527:function(a,i){}}]);
+//# sourceMappingURL=6.7668d156.chunk.js.map
